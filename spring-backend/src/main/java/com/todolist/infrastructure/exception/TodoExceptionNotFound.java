68b7427715d2e07465f060242cb20fbbdd40bbfc
Matheus Martins <125091690+Matheus-Martins-Souza-Silva@users.noreply.github.com>
2026-03-01 13:29:51 -0300

    Updated error message

--- spring-backend/src/main/java/com/todolist/infrastructure/exception/TodoExceptionNotFound.java
+++ spring-backend/src/main/java/com/todolist/infrastructure/exception/TodoExceptionNotFound.java
@@ -3,7 +3,7 @@ package com.todolist.infrastructure.exception;
 public class TodoExceptionNotFound extends RuntimeException {
 
     public TodoExceptionNotFound() {
-        super("Erro ao enviar ao envio dos dados da todo-list.");
+        super("Error on send todo list.");
     }
 
     public TodoExceptionNotFound(String msg) {
