b636ec8a3e2ddf68f6a0eb602adbe9f99e076cde
Matheus Martins <125091690+Matheus-Martins-Souza-Silva@users.noreply.github.com>
2026-03-01 13:30:16 -0300

    Remove unused Todo mapping methods

--- spring-backend/src/main/java/com/todolist/util/mappers/TodoMapper.java
+++ spring-backend/src/main/java/com/todolist/util/mappers/TodoMapper.java
@@ -10,28 +10,6 @@ import com.todolist.domain.entities.todo.TodoStatus;
 @Component
 public class TodoMapper {
 
-    /*public TodoDTO toDTO(Todo todo)
-    {
-        if(todo == null)
-            return null;
-
-        return new TodoDTO(todo.getTitle(), todo.getStatus().getValue());
-    }
-
-    public Todo toEntity(TodoDTO todoDTO)
-    {
-        if(todoDTO == null)
-            return null;
-
-        Todo todo = new Todo();
-
-        todo.setId(new TodoID());
-        todo.setTitle(todoDTO.title());
-        todo.setStatus(TodoStatus.fromValue(todoDTO.status()));
-
-        return todo;
-    }*/
-
     public Todo toEntity(CreateTodoRequest todoRequest) {
         if(todoRequest == null)
             return null;
